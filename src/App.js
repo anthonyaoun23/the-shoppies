ddf30372de9f0a865d8113ef35cfced5ff3a97ba
Anthony Aoun <anthony.aoun1@hotmail.com>
2021-01-12 12:54:57 -0500

    feat: add search bar, search icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 import "styles/App.css";
+import searchIcon from "assets/icons/search.svg";
 
 function App() {
+  const [movieQuery, setMovieQuery] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [queryResults, setQueryResults] = useState(0);
+
+  const handleChange = ({ target }) => {
+    setMovieList([]);
+    setQueryResults(0);
+    setIsError(false);
+    setMovieQuery(String(target.value).trim());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">Header</header>
-      <main>Main</main>
+    <div className="app">
+      <header className="app-header">The Shoppies 2021</header>
+      <main>
+        <section className="search-container">
+          <div className="icon-search">
+            <img src={searchIcon} className="icon" alt="Search Icon" />
+          </div>
+
+          <input
+            onChange={handleChange}
+            className="input"
+            type="text"
+            placeholder="Search for a movie"
+            value={movieQuery}
+            autoFocus
+            required
+          />
+        </section>
+      </main>
       <footer>Footer</footer>
     </div>
   );
